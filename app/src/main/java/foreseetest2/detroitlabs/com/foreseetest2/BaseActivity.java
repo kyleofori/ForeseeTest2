53250c677e3245e8450de795a10c456028c07682
Stuart Kent <stuart@detroitlabs.com>
2015-03-31 13:09:49 -0400

    Add translucent window flag to theme, issue still not reproduced.

--- app/src/main/java/foreseetest2/detroitlabs/com/foreseetest2/BaseActivity.java
+++ app/src/main/java/foreseetest2/detroitlabs/com/foreseetest2/BaseActivity.java
@@ -1,6 +1,7 @@
 package foreseetest2.detroitlabs.com.foreseetest2;
 
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import com.foresee.sdk.ForeSee;
@@ -10,18 +11,22 @@ public class BaseActivity extends ActionBarActivity {
     @Override
     public void onStart() {
         super.onStart();
+        Log.d(BaseActivity.class.getSimpleName(), "onStart");
         ForeSee.activityStarted(this);
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Log.d(BaseActivity.class.getSimpleName(), "onPause");
         ForeSee.activityPaused(this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(BaseActivity.class.getSimpleName(), "onResume");
+
         ForeSee.activityResumed(this);
 
         final ViewGroup rootViewGroup = (ViewGroup) ((ViewGroup) this.findViewById(android.R.id.content)).getChildAt(0);
